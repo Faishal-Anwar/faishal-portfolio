a91799942938beb7af9c61d996575c44e83cc840
Faisal61919112 <catatanfaishal@gmail.com>
2026-04-26 00:16:50 +0700

    fix: revert cache store to database and Tailwind to CDN to fix 500 error

--- config/database.php
+++ config/database.php
@@ -61,7 +61,6 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_PERSISTENT => true,
             ]) : [],
         ],
 
@@ -82,7 +81,6 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_PERSISTENT => true,
             ]) : [],
         ],
 
