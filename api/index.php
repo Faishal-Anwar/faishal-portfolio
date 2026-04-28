508e9ecb6d0eeeb089574a7f96fa5586405a837c
Faisal61919112 <catatanfaishal@gmail.com>
2026-04-28 23:26:04 +0700

    fix: add @vercel/static builds for CSS/JS assets

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
 
-// Set compiled views path for Vercel serverless
-if (getenv('VERCEL')) {
-    $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-    $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-    $_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-    $_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
-    
-    // Ensure tmp directories exist
-    if (!is_dir('/tmp/views')) {
-        mkdir('/tmp/views', 0755, true);
-    }
-}
-
 require __DIR__ . '/../public/index.php';
