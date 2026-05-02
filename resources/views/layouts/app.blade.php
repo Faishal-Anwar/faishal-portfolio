d839ade80020403072f564b87bff30e451229c4d
Faishal-Anwar <150545655+Faishal-Anwar@users.noreply.github.com>
2026-05-02 11:07:13 +0700

    Change font size from 17px to 16px

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,11 +79,11 @@
         html { 
             overflow-y: scroll; 
             scrollbar-gutter: stable; 
-            font-size: 17px; 
+            font-size: 16px; 
         }
         
         @media (min-width: 1024px) {
-            html { font-size: 17px; } 
+            html { font-size: 16px; } 
             :root { --sidebar-w: 20rem; }
         }
 
